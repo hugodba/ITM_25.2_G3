0bf5e317e07262f3a7439b03632eedcecb3593cb
Andre-FP <andre.pereira@poli.ufrj.br>
2025-10-22 02:10:45 -0300

    Add general program. It defines Element, Circuit, and the main program in an object-oriented way.

--- src/circuit_simulator/Circuit.py
+++ src/circuit_simulator/Circuit.py
@@ -1,10 +1,184 @@
-class Circuit:
-    """Base class for circuits."""
+from .Element import Element
+import numpy as np
+
+
+class Circuit:    
+    """Class representing an electrical circuit."""
     def __init__(self):
+        self.n = None
+        self.Gn = None
+        self.In = None
+        self.e = []
+        self.e0 = [] # Initial condition of nodal voltages (time t - deltaT)
+        self.w = 0
+        self.components = []
+        self.Idc = []
+        self.Isin = []
+        self.Vsin = []
+        self.R = []
+        self.GmI = []
+        self.C = []
+        self.L = []
+        self.K = []
+        self.vars = []        # Name variables of interests (currents)
+        self.vars_values = []
+        self.i_values = []
+        self.i_values0 = [] # Initial condition of i variables (time t - deltaT)
+        self.i_vars0 = {} # Initial condition of i variables associating name and values
+        self.deltaT = None 
+        self.period = None
         self.elements = []
 
-    def add_element(self, element):
-        self.elements.append(element)
 
-    def __str__(self):
-        return f"Circuit with {len(self.elements)} elements."
+    def get_number_of_nodes(self, content):
+        """
+        Get number of nodes for creating the Gn and In matrices.
+        Eliminates comments and blank lines.
+
+        Args:
+            content (list): Full content of netlist file in "readlines" format.
+
+        Returns:
+            [int]: Number of nodes.
+            [list]: Content of netlist file with useless line removed.
+        """
+        # TODO: Adapt this function for the current project
+        
+        # Store only unique elements
+        nodes = set()
+        
+        i = 0
+        content_lenght = len(content)
+        while(i < content_lenght):
+            # Skip and removes blank lines and comments:       
+            if content[i][0] == "\n" or content[i][0] == "*" or content[i][0] == " "\
+            or content[i][0] == "\0": 
+                
+                content.remove(content[i])
+
+                # The next element becomes the current one, so doesn't need to add 1 in "i".
+                # Content lenght down by 1 
+                content_lenght -= 1
+
+            elif content[i][0] == "I" or content[i][0] == "R" or content[i][0] == "G"\
+                or content[i][0] == "L" or content[i][0] == "C" or content[i][0] == "K"\
+                or content[i][0] == "F" or content[i][0] == "E" or content[i][0] == "H"\
+                or content[i][0] == "V" or content[i][0] == "D":
+
+                splitted_line = content[i].split(" ")
+                
+                # Add node "a" and "b"
+                nodes.add(int(splitted_line[1]))
+                nodes.add(int(splitted_line[2]))
+
+                if content[i][0] == "G" or content[i][0] == "K" or content[i][0] == "F"\
+                    or content[i][0] == "E" or content[i][0] == "H":
+
+                    # nodes "c" and "d" (control nodes for "G", and nodes for the secondary 
+                    # inductor of the transformer "K")
+                    nodes.add(int(splitted_line[3]))
+                    nodes.add(int(splitted_line[4]))
+
+                # Get frequency of sine wave source
+                elif splitted_line[3] == "SIN":
+                    self.w = 2*np.pi*float(splitted_line[6])        
+                i += 1
+            
+            else:
+                raise ValueError(f"\nNetlist com a {i}ª linha inválida")
+
+        self.n = len(nodes)
+        return self.n, content
+
+
+    def add_components(
+            self, Yn, In, content, elem_type, method="backward", 
+            deltaT=None, e0=None, i_vars0=None, time=None
+        ):
+        """Calls "Element" class to create object according to the circuit element.
+        It also executes its stamps and add it in the Yn and In matrices
+
+        Args:
+            Yn (np.ndarray): Admitance matrix
+            In (np.ndarray): Current vector
+            content (list): Full content of netlist file in "readlines" format.
+            elem_type (str): "invariant", "variant" or "non_linear". It defines
+                the type of the element to gather execution
+            method (str): "backward", "forward" or "trapezio". It defines the method
+                of integration for definition of the stamp.
+            deltaT (float): Step time (s).
+            e0 (np.ndarray): Array of nodal voltages of last time step.
+            i_vars0 (np.ndarray): Array of currents of last time step from modified 
+                nodal analysis. 
+            time (int): current time (s)
+            
+        Returns:
+            np.ndarray, np.ndarray: new Yn and In matrix 
+        """
+
+        for component in content:
+            self.elements.append(
+                Element.create(
+                    component.split(" "), Yn, In, method, elem_type, 
+                    deltaT=deltaT, e0=e0, i_vars0=i_vars0, time=time
+                )
+            )
+        return Yn, In
+
+
+    def readlines_netlist(self, netlist_path: str):
+        # Get the content of the netlist file
+        try:
+            with open(netlist_path) as netfile:
+                content = netfile.readlines()
+        except:
+            raise ValueError("Erro. Caminho da netlist inválida.")
+        return content
+
+    def analyze(self, netlist, period, step, max_iter, tolerance):
+        """Calculates the nodal voltages and variables of interest. 
+        Stores values in attributes "e", "i_values", "vars_values".
+
+        Args:
+            netlist (str): Path for netlist file.
+
+        Returns:
+            np.ndarray: nodal voltages vector 
+        """
+        # Refresh all variables
+        self.__init__()
+        
+        # Save delta(t) and period
+        self.deltaT = step
+        self.period = period
+
+        # Lê a netlist e transforma em lista
+        content = self.readlines_netlist(netlist)
+
+        # Get the number of nodes for creating Gn and In
+        n, content = self.get_number_of_nodes(content) 
+        Gn = np.zeros((n, n))
+        In = np.zeros((n, 1))
+
+        # Add time invariant components
+        invariant_Gn, invariant_In = self.add_components(Gn, In, content, "invariant")
+        
+        for time in np.arange(0.0, period, step):
+            self.vars = [] # Because add_components_time_variant adds the same vars each time
+
+            # Add time variant components
+            Gn_variant, In_variant = self.add_components(
+                invariant_Gn, 
+                invariant_In, 
+                content,
+                "variant",
+                deltaT=step,
+                e0=self.e0,
+                time=time
+            )
+
+            # TODO: Adicionar Newton Raphson para os componentes não lineares
+
+            # TODO: Resolver o sistema matricial de equações 
+
+        return self.e
--- src/circuit_simulator/Element.py
+++ src/circuit_simulator/Element.py
@@ -1,7 +1,75 @@
+import numpy as np
+
 class Element:
-    """Base class for circuit elements."""
-    def __init__(self, name: str):
-        self.name = name
+    """Classe base para elementos de circuito elétrico."""
+
+    registry = {}  # Dicionário de elementos filhos registrados
+
+    def __init_subclass__(cls, prefix=None, **kwargs):
+        """
+        Método chamado automaticamente quando uma subclasse de Element é criada.
+        Se a subclasse fornecer um prefixo, ela é registrada automaticamente.
+        """
+        super().__init_subclass__(**kwargs)
+        if prefix is not None:
+            Element.registry[prefix.lower()] = cls
+
+    def __init__(self, splitline_netlist, Yn, In, method="backward"):
+        self.name = splitline_netlist[0]   # Ex: "R1"
+        self.splitline_netlist = splitline_netlist
+        self.Yn = Yn                      # Matriz de admitâncias do circuito
+        self.In = In                      # Vetor de correntes injetadas
+        self.method = method              # Método numérico (ex: "backward")
+
+    @staticmethod
+    def create(splitline_netlist, Yn, In, method="backward", elem_type=None, **kwargs):
+        """
+        Cria automaticamente a instância correta do elemento com base no prefixo.
+        Exemplo: "R1" -> classe Resistor.
+        Args: 
+            elem_type (str): "invariant", "variant" or "non_linear". Descreve o 
+                tipo do elemento, para criar o elemento da netlist apenas se for 
+                do mesmo tipo requisitado.   
+        """
+
+        prefix = splitline_netlist[0][0].lower()  # Ex: "r" em "R1"
+        cls = Element.registry.get(prefix)
+        if cls is None:
+            raise ValueError(f"Tipo de elemento desconhecido: {splitline_netlist[0]}")
+        
+        # Retorna o elemento após executado somente se for do tipo desejado.
+        # Motivo: criar os invariantes no tempo, variantes e os não lineares 
+        # separadamente. 
+        if (elem_type is None) or (cls.elem_type == elem_type):
+            return cls(splitline_netlist, Yn, In, method, **kwargs)
+
+    def add_stamp_backward(self):
+        raise NotImplementedError("Subclasses devem implementar add_stamp_backward().")
+
+    def add_stamp_forward(self):
+        raise NotImplementedError("Subclasses devem implementar add_stamp_forward().")
+
+    def add_stamp_trapezio(self):
+        raise NotImplementedError("Subclasses devem implementar add_stamp_trapezio().")
+
+    def add_stamp(self, method):
+        if method == "backward":
+            self.add_stamp_backward(self)
+        elif method == "forward":
+            self.add_stamp_forward(self)
+        elif method == "trapezio":
+            self.add_stamp_trapezio(self)
+
+    def add_newblanklines_YnIn(self, newlines=1):
+        n = len(self.Yn)
+        newYn = np.zeros((n + newlines, n + newlines))
+        newIn = np.zeros((n + newlines, 1))
+        newYn[:n, :n] = self.Yn[:, :]
+        newIn[:n] = self.In[:]
+        
+        self.Yn = newYn
+        self.In = newIn
+
 
     def __str__(self):
-        return f"Element: {self.name}"
+        return f"Elemento genérico {self.name}"
